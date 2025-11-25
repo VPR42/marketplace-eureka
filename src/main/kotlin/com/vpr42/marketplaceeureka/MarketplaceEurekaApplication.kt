c58069afd3f97354dde0444fd2b4ebfd4d5e7ea9
Запара Иван <truthpruthbg@gmail.com>
2025-11-25 15:12:19 +0300

    INIT: Инициализация проекта

--- src/main/kotlin/com/vpr42/marketplaceeureka/MarketplaceEurekaApplication.kt
+++ src/main/kotlin/com/vpr42/marketplaceeureka/MarketplaceEurekaApplication.kt
@@ -1,11 +1,11 @@
-package com.vpr42.kotlintemplaterepository
+package com.vpr42.marketplaceeureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinTemplateRepositoryApplication
+class MarketplaceEurekaApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinTemplateRepositoryApplication>(*args)
+    runApplication<MarketplaceEurekaApplication>(*args)
 }
